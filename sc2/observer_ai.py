8084ba6f5375dc26a90263a04e14a87a6ba3d5aa
Matuiss2 <mtsbrbr@gmail.com>
2020-05-03 16:25:23 -0300

    codacy fixes part 20 -> 'fix duplications part16' -> build a mother class AI to house repeated stuff from observer_ai.py and bot_ai.py part 3

--- sc2/observer_ai.py
+++ sc2/observer_ai.py
@@ -36,12 +36,6 @@ class ObserverAI(Ai, DistanceCalculation):
     def __init__(self):
         DistanceCalculation.__init__(self)
         Ai.__init__(self)
-        self.enemy_structures: Units = Units([], self)
-        self.resources: Units = Units([], self)
-        self.destructible: Units = Units([], self)
-        self.watchtowers: Units = Units([], self)
-        self.mineral_field: Units = Units([], self)
-        self.vespene_geyser: Units = Units([], self)
         self.larva: Units = Units([], self)
         self.techlab_tags: Set[int] = set()
         self.reactor_tags: Set[int] = set()
