938e238b766c568b1399feabbc11efa8172cd385
Matuiss2 <mtsbrbr@gmail.com>
2020-05-04 21:03:47 -0300

    codacy fixes part 25 -> 'fix duplications part20' -> build a mother class AI to house repeated stuff from observer_ai.py and bot_ai.py part 7 (not sure if this is done the best way)

--- sc2/ai.py
+++ sc2/ai.py
@@ -3,32 +3,15 @@ Mother class for the bot_ai and observer_ai
 """
 from typing import Set
 
-from .game_state import Blip
-from .units import Units
 from .ids.upgrade_id import UpgradeId
+from sc2.units_to_prepare import UnitsToPrepare
 
 
-class Ai:
+class Ai(UnitsToPrepare):
     def __init__(self):
+        super().__init__()
         self.opponent_id: str = None
         self.realtime: bool = False
-        self.all_units: Units = Units([], self)
-        self.units: Units = Units([], self)
-        self.workers: Units = Units([], self)
-        self.townhalls: Units = Units([], self)
-        self.structures: Units = Units([], self)
-        self.gas_buildings: Units = Units([], self)
-        self.enemy_units: Units = Units([], self)
-        self.enemy_structures: Units = Units([], self)
-        self.resources: Units = Units([], self)
-        self.destructible: Units = Units([], self)
-        self.watchtowers: Units = Units([], self)
-        self.mineral_field: Units = Units([], self)
-        self.vespene_geyser: Units = Units([], self)
-        self.larva: Units = Units([], self)
-        self.techlab_tags: Set[int] = set()
-        self.reactor_tags: Set[int] = set()
-        self.blips: Set[Blip] = set()
         self.minerals: int = 0
         self.vespene: int = 0
         self.supply_army: float = 0
@@ -39,23 +22,7 @@ class Ai:
         self._previous_upgrades: Set[UpgradeId] = set()
 
     def _prepare_units(self):
-        self.blips: Set[Blip] = set()
-        self.units: Units = Units([], self)
-        self.structures: Units = Units([], self)
-        self.enemy_units: Units = Units([], self)
-        self.enemy_structures: Units = Units([], self)
-        self.mineral_field: Units = Units([], self)
-        self.vespene_geyser: Units = Units([], self)
-        self.resources: Units = Units([], self)
-        self.destructible: Units = Units([], self)
-        self.watchtowers: Units = Units([], self)
-        self.all_units: Units = Units([], self)
-        self.workers: Units = Units([], self)
-        self.townhalls: Units = Units([], self)
-        self.gas_buildings: Units = Units([], self)
-        self.larva: Units = Units([], self)
-        self.techlab_tags: Set[int] = set()
-        self.reactor_tags: Set[int] = set()
+        super().__init__()
 
     async def _issue_upgrade_events(self):
         difference = self.state.upgrades - self._previous_upgrades
